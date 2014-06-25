174a871f397aba23e1fb722131edb8ac7738a27f
Timothy O'Shea <dylan.jc.oshea@gmail.com>
2014-06-25 01:25:21 -0700

    Massive changes -- designed and added a logo, tried several new carousels and added a shadow to the home page carousel

--- js/glp.js
+++ js/glp.js
@@ -3,15 +3,38 @@ $(document).ready(function(){
     $(this).children('.dropgroup').slideDown(80);
   })
   $('.dropdown').on('mouseleave', function(){
-    $(this).children('.dropgroup').slideUp(80);
+    // $(this).children('.dropgroup').slideUp(80);
   })
-  $('.slick').slick({
-    autoplay: true,
-    autoplaySpeed: 5000,
-    fade: true,
-    dots: true,
-    speed: 1000,
-    slide: 'img'
+  // $('.slick').slick({
+  //   autoplay: true,
+  //   autoplaySpeed: 5000,
+  //   fade: true,
+  //   dots: true,
+  //   speed: 1000,
+  //   slide: 'img'
+
+  // });
+
+  $(document).ready(function() {
+
+    // $(".owl-carousel").owlCarousel({
+    //   items: 1,
+    //   loop: true,
+    //   autoplay: true,
+    //   autoplaySpeed: 1000
+    // });
+
+
+    $('.slick').slick({
+      autoplay: true,
+      autoplaySpeed: 5000,
+      fade: true,
+      // dots: true,
+      arrows: false,
+      speed: 1000,
+      slide: 'img'
+
+    });
 
   });
 });
